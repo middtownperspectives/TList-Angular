20f4e34a3c53e6b96892f0dcc3cef2117932efac
middtown perspectives <middtownperspectives@gmail.com>
2019-05-12 20:12:27 -0600

    few more to go

--- src/app/components/faq-tree/example-data.ts
+++ src/app/components/faq-tree/example-data.ts
@@ -1,47 +1,34 @@
 /** Example file/folder data. */
 export const files = [
   {
-    name: 'material2',
+    name: 'Am I ever able to speak to a tea expert?',
     type: 'folder',
     children: [
-      {
-        name: 'src',
-        type: 'folder',
-        children: [
-          {
-            name: 'cdk',
-            type: 'folder',
-            children: [
-              { name: 'package.json', type: 'file' },
-              { name: 'BUILD.bazel', type: 'file' },
-            ]
-          },
-          { name: 'lib', type: 'folder' }
-        ]
-      }
+      { name: `The T-List's customer service representatives are available Teusday-Thursday
+               from 10am to 2pm (Mountain Time).`, type: 'file',
+    },
+      { name: `Hit us up on Twitter or Instagram for other communications`,  type: 'file'},
     ]
   },
   {
-    name: 'angular',
+    name: 'What are the Health Benefits of Tea?',
     type: 'folder',
     children: [
-      {
-        name: 'packages',
-        type: 'folder',
-        children: [
-          { name: '.travis.yml', type: 'file' },
-          { name: 'firebase.json', type: 'file' }
-        ]
+      { name: `Green tea, black tea, white tea, and oolong tea all have antioxidants.`, type: 'file'},
+      { name: `These antioxidants are polyphenols, specifically catechins. Studies from institutes like Purdue, Linus Pauling,
+      Harvard and the Medical College of Wisconsin have shown a variety of benefits from drinking tea – greater immunity,
+      improved cognitive functions, a decreased risk of cardiovascular problems, less fatigue, lower blood pressure, a slowing of
+      the aging process, and reduced stress. Recent research from Shandong University in China concluded that green tea may even help
+      ease depression.`, type: 'file',
       },
-      { name: 'package.json', type: 'file' }
     ]
   },
   {
-    name: 'angularjs',
+    name: 'Where are your Teas sourced?',
     type: 'folder',
     children: [
-      { name: 'gulpfile.js', type: 'file' },
-      { name: 'README.md', type: 'file' }
+      { name: `The Caribean and the rural providences of Japan and India.`, type: 'file',
+      },
     ]
   }
 ];
