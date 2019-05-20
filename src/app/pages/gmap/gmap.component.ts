a2544c6e4942dabb6d0675134fe15c5c9242a1f8
middtown <aarontm9@gmail.com>
2019-05-20 01:31:55 -0500

    google map working

--- src/app/pages/gmap/gmap.component.ts
+++ src/app/pages/gmap/gmap.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class GmapComponent implements OnInit {
 
   public title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 39.729300;
+  lng = -104.939252;
+
+  public steepTeaAndCoffee = [39.729300, -104.939252];
+  public coffee = [39.734232, -104.987659];
+  public denverTeaRoom = [39.696298, -104.987567];
+  public capitolTea = [39.690236, -104.987235];
 
   constructor() { }
 
